48e886a8303ba390f2ac58deb84562c059a31509
Koushik <Koushikbajpayee06@gmail.com>
2026-02-12 18:17:10 +0530

    bug fix

--- src/routes/user.js
+++ src/routes/user.js
@@ -27,9 +27,14 @@ userRouter.get('/user/requests/connections', jwtAuth, async(req,res)=>{
             {toUserId: loggedInUser._id, status:"accepted"},
             {fromUserId: loggedInUser._id, status:"accepted"}
         ]
-    }).populate("fromUserId" ,USER_SAFE_DATA);
+    }).populate("fromUserId" ,USER_SAFE_DATA).populate("toUserId", USER_SAFE_DATA);
 
-    const data = connectionRequests.map((row) => row.fromUserId)
+    const data = connectionRequests.map((row) => {
+        if(row.fromUserId._id.toString() === loggedInUser._id.toString()){
+            return row.toUserId
+        }
+        return row.fromUserId
+    })
     res.json({message:"Connections fetch Succesfull", data:data})
     }catch(err){
         res.status(400).send({message:err.message})
